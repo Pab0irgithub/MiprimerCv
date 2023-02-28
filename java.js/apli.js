6fdb2ed54045c6a5d9824d4e3455581c15c6ee59
pabloiriarte <pabloiriarte27@gmail.com>
2023-02-27 23:17:00 -0600

    first commit

---
+++ java.js/apli.js
@@ -0,0 +1,35 @@
+//menu lateral
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarmenu(){
+    if(menu_visible==false){
+        menu.style.display = "block"
+        menu_visible = true;
+    }
+ else{
+    menu.style.display = "none"
+    menu_visible = false
+ }
+
+
+}
+//
+</header>
+
+<div class="info">
+  
+   <h1>Miguel Garza</h1>
+    <h2>estudiante, programacion, front-end</h2>
+     <div class="redes">
+       <a href="#"><i class="fa-brands fa-instagram"></i></a>
+       <a href="#"><i class="fa-brands fa-facebook"></i></a>
+       <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+     </div>
+ </div>
+ <div class="foto">
+    <img src="./CSS/img/nuevo avatar.jpg" alt="imagen de la foto">
+ </div>
+    </div>
+
+//
+
